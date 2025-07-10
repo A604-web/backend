d2867d6e3c2b825e6d4deefec93b1ae8a4baa1ab
cheesecrust <jack0810@naver.com>
2025-07-10 17:47:30 +0900

    Internal: deploy.yml

--- src/main/java/com/ssafy/db/entity/User.java
+++ src/main/java/com/ssafy/db/entity/User.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
